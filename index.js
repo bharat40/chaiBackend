c2b8ddd8688930a9f00e03d018b67d994794cb30
bharat <bharatdhiman40@gmail.com>
2025-02-01 19:10:56 +0530

    github data added

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 import e from "express";
 import 'dotenv/config'
 const app = e();
-const port = 4000
+
+const githubData = {
+    "login": "bharat40",
+    "id": 119802463,
+    "node_id": "U_kgDOByQKXw",
+    "avatar_url": "https://avatars.githubusercontent.com/u/119802463?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/bharat40",
+    "html_url": "https://github.com/bharat40",
+    "followers_url": "https://api.github.com/users/bharat40/followers",
+    "following_url": "https://api.github.com/users/bharat40/following{/other_user}",
+    "gists_url": "https://api.github.com/users/bharat40/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/bharat40/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/bharat40/subscriptions",
+    "organizations_url": "https://api.github.com/users/bharat40/orgs",
+    "repos_url": "https://api.github.com/users/bharat40/repos",
+    "events_url": "https://api.github.com/users/bharat40/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/bharat40/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Bharat",
+    "company": null,
+    "blog": "",
+    "location": "Ambala Cantt, Haryana, India",
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 47,
+    "public_gists": 0,
+    "followers": 3,
+    "following": 2,
+    "created_at": "2022-12-04T15:25:37Z",
+    "updated_at": "2025-01-29T16:18:34Z"
+}
 
 
 app.get('/', (req, res) => {
@@ -18,6 +53,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send('<h2> Chai aur code </h2>')
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
